57050022ffa4763a6f3c16124931519b6d1d4b02
patelaftab <10patelaftab@gmail.com>
2023-03-05 22:53:36 +0530

    [Aftab] : Creating Binary Tree By Adding Elements

---
+++ BinarySearchTreeHashTable/BSTClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeHashTable
+{
+    internal class BinarySearchTreeClass
+    {
+        Node root;
+        public void Insert(Node node)
+        {
+            root = InsertHelper(root, node);
+        }
+        public Node InsertHelper(Node root, Node node)
+        {
+            int data = node.data;
+            if (root == null)
+            {
+                root = node;
+                return root;
+            }
+            else if (data < root.data)
+            {
+                root.left = InsertHelper(root.left, node);
+            }
+            else
+            {
+                root.right = InsertHelper(root.right, node);
+            }
+            return root;
+        }
+        public void Display()
+        {
+            DisplayHelper(root);
+        }
+        public void DisplayHelper(Node root)
+        {
+            if (root != null)
+            {
+                DisplayHelper(root.left);
+                Console.Write(root.data + " ");
+                DisplayHelper(root.right);
+                return;
+            }
+        }
+    }
+}
---
+++ BinarySearchTreeHashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeHashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hash Table And Binary Search Tree Problem");
+            BinarySearchTreeClass BST = new BinarySearchTreeClass();
+            BST.Insert(new Node(56));
+            BST.Insert(new Node(30));
+            BST.Insert(new Node(70));
+            Console.Write("Binary Search Tree :");
+            BST.Display();
+            Console.ReadLine();
+        }
+
+    }
+}
